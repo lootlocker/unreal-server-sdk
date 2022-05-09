1d97ce807c0c3fc43607135d44a7ba24f2ad6521
Enno Rehling <enno.rehling@gmail.com>
2022-05-09 13:17:18 +0200

    bugfixes and code style
    UploadFile was using the wrong Token.
    UploadFileForPlayer using the wrong endpoint
    Naming variables the UE way.

--- 5.00/DemoProject/Plugins/LootLockerServerSDK/Source/LootLockerServerSDK/Private/ServerAPI/LootLockerServerFilesRequest.cpp
+++ 5.00/DemoProject/Plugins/LootLockerServerSDK/Source/LootLockerServerSDK/Private/ServerAPI/LootLockerServerFilesRequest.cpp
@@ -16,122 +16,116 @@ ULootLockerServerFilesRequest::ULootLockerServerFilesRequest()
 void ULootLockerServerFilesRequest::ListFilesForPlayer(int PlayerId,
     const FListFilesForPlayerResponseBP& OnCompletedRequestBP, const FListFilesForPlayerResponse& OnCompletedRequest)
 {
-    FServerResponseCallback sessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse response)
+    const FServerResponseCallback sessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse Response)
         {
             FLootLockerServerListFilesForPlayerResponse ResponseStruct;
-            if (response.success)
+            if (Response.success)
             {
                 ResponseStruct.success = true;
-                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerListFilesForPlayerResponse>(response.FullTextFromServer, &ResponseStruct, 0, 0);
+                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerListFilesForPlayerResponse>(Response.FullTextFromServer, &ResponseStruct, 0, 0);
 
             }
             else {
                 ResponseStruct.success = false;
                 UE_LOG(LogTemp, Error, TEXT("Listing player files from lootlocker failed"));
             }
-            ResponseStruct.FullTextFromServer = response.FullTextFromServer;
+            ResponseStruct.FullTextFromServer = Response.FullTextFromServer;
             OnCompletedRequestBP.ExecuteIfBound(ResponseStruct);
             OnCompletedRequest.ExecuteIfBound(ResponseStruct);
         });
 
-    FString ContentString;
-    FLootLockerServerEndPoints Endpoint = ULootLockerServerGameEndpoints::ListFilesForPlayerEndpoint;
-    FString endpoint = FString::Format(*(Endpoint.endpoint), { PlayerId });
-    FString requestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(Endpoint.requestMethod));
+    FLootLockerServerEndPoints ServerEndpoint = ULootLockerServerGameEndpoints::ListFilesForPlayerEndpoint;
+    const FString Endpoint = FString::Format(*(ServerEndpoint.endpoint), { PlayerId });
+    const FString RequestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(ServerEndpoint.requestMethod));
 
-    UE_LOG(LogTemp, Log, TEXT("data=%s"), *ContentString);
-    HttpClient->SendApi(endpoint, requestMethod, ContentString, sessionResponse, true);
+    const FString ContentString;
+    HttpClient->SendApi(Endpoint, RequestMethod, ContentString, sessionResponse, true);
 }
 
 void ULootLockerServerFilesRequest::GetFileByIdForPlayer(int PlayerId, int FileId,
     const FGetFileByIdForPlayerResponseBP& OnCompletedRequestBP, const FGetFileByIdForPlayerResponse& OnCompletedRequest)
 {
-    FServerResponseCallback sessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse response)
+    const FServerResponseCallback sessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse Response)
         {
             FLootLockerServerGetFileByIdForPlayerResponse ResponseStruct;
-            if (response.success)
+            if (Response.success)
             {
                 ResponseStruct.success = true;
-                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerPlayerFile>(response.FullTextFromServer, &ResponseStruct.item, 0, 0);
+                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerPlayerFile>(Response.FullTextFromServer, &ResponseStruct.item, 0, 0);
 
             }
             else {
                 ResponseStruct.success = false;
                 UE_LOG(LogTemp, Error, TEXT("Getting player file from lootlocker failed"));
             }
-            ResponseStruct.FullTextFromServer = response.FullTextFromServer;
+            ResponseStruct.FullTextFromServer = Response.FullTextFromServer;
             OnCompletedRequestBP.ExecuteIfBound(ResponseStruct);
             OnCompletedRequest.ExecuteIfBound(ResponseStruct);
         });
 
-    FString ContentString;
-    FLootLockerServerEndPoints Endpoint = ULootLockerServerGameEndpoints::GetFileByIdForPlayerEndpoint;
-    FString endpoint = FString::Format(*(Endpoint.endpoint), { PlayerId, FileId });
-    FString requestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(Endpoint.requestMethod));
+    FLootLockerServerEndPoints ServerEndpoint = ULootLockerServerGameEndpoints::GetFileByIdForPlayerEndpoint;
+    const FString Endpoint = FString::Format(*(ServerEndpoint.endpoint), { PlayerId, FileId });
+    const FString RequestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(ServerEndpoint.requestMethod));
 
-    UE_LOG(LogTemp, Log, TEXT("data=%s"), *ContentString);
-    HttpClient->SendApi(endpoint, requestMethod, ContentString, sessionResponse, true);
+    const FString ContentString;
+    HttpClient->SendApi(Endpoint, RequestMethod, ContentString, sessionResponse, true);
 }
 
 void ULootLockerServerFilesRequest::UploadFileForPlayer(int PlayerId, const FString& FilePath, const FString& Purpose,
     const FUploadFileForPlayerResponseBP& OnCompletedRequestBP, const FUploadFileForPlayerResponse& OnCompletedRequest)
 {
-    FServerResponseCallback sessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse response)
+    const FServerResponseCallback SessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse Response)
         {
             FLootLockerServerUploadFileForPlayerResponse ResponseStruct;
-            if (response.success)
+            if (Response.success)
             {
                 ResponseStruct.success = true;
-                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerPlayerFile>(response.FullTextFromServer, &ResponseStruct.item, 0, 0);
+                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerPlayerFile>(Response.FullTextFromServer, &ResponseStruct.item, 0, 0);
 
             }
             else {
                 ResponseStruct.success = false;
                 UE_LOG(LogTemp, Error, TEXT("Uploading player file to lootlocker failed"));
             }
-            ResponseStruct.FullTextFromServer = response.FullTextFromServer;
+            ResponseStruct.FullTextFromServer = Response.FullTextFromServer;
             OnCompletedRequestBP.ExecuteIfBound(ResponseStruct);
             OnCompletedRequest.ExecuteIfBound(ResponseStruct);
         });
 
-    FString ContentString;
-    FLootLockerServerEndPoints Endpoint = ULootLockerServerGameEndpoints::GetFileByIdForPlayerEndpoint;
-    FString endpoint = FString::Format(*(Endpoint.endpoint), { PlayerId });
-    FString requestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(Endpoint.requestMethod));
+    const FLootLockerServerEndPoints ServerEndpoint = ULootLockerServerGameEndpoints::UploadFileForPlayerEndpoint;
+    const FString Endpoint = FString::Format(*(ServerEndpoint.endpoint), { PlayerId });
+    const FString RequestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(ServerEndpoint.requestMethod));
     TMap<FString, FString> AdditionalFields;
     if (!Purpose.IsEmpty()) {
         AdditionalFields.Add("purpose", Purpose);
     }
 
-    UE_LOG(LogTemp, Log, TEXT("data=%s"), *ContentString);
-    HttpClient->UploadFile(endpoint, requestMethod, FilePath, AdditionalFields, sessionResponse, true);
+    HttpClient->UploadFile(Endpoint, RequestMethod, FilePath, AdditionalFields, SessionResponse, true);
 }
 
 void ULootLockerServerFilesRequest::DeleteFileForPlayer(int PlayerId, int FileId, const FDeleteFileForPlayerResponseBP& OnCompletedRequestBP, const FDeleteFileForPlayerResponse& OnCompletedRequest)
 {
-    FString ContentString;
-
-    FServerResponseCallback sessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse response)
+    const FServerResponseCallback SessionResponse = FServerResponseCallback::CreateLambda([OnCompletedRequestBP, OnCompletedRequest](FLootLockerServerResponse Response)
         {
             FLootLockerServerDeleteFileForPlayerResponse ResponseStruct;
-            if (response.success)
+            if (Response.success)
             {
-                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerDeleteFileForPlayerResponse>(response.FullTextFromServer, &ResponseStruct, 0, 0);
+                FJsonObjectConverter::JsonObjectStringToUStruct<FLootLockerServerDeleteFileForPlayerResponse>(Response.FullTextFromServer, &ResponseStruct, 0, 0);
                 ResponseStruct.success = true;
             }
             else {
                 ResponseStruct.success = false;
                 UE_LOG(LogTemp, Error, TEXT("DeleteFileForPlayer failed from lootlocker"));
             }
-            ResponseStruct.FullTextFromServer = response.FullTextFromServer;
+            ResponseStruct.FullTextFromServer = Response.FullTextFromServer;
             OnCompletedRequestBP.ExecuteIfBound(ResponseStruct);
             OnCompletedRequest.ExecuteIfBound(ResponseStruct);
         });
 
-    FLootLockerServerEndPoints Endpoint = ULootLockerServerGameEndpoints::DeleteFileForPlayerEndpoint;
-    FString TempEndpoint = Endpoint.endpoint;
-    FString newEndpoint = FString::Format(*TempEndpoint, { PlayerId, FileId });
+    FLootLockerServerEndPoints ServerEndpoint = ULootLockerServerGameEndpoints::DeleteFileForPlayerEndpoint;
+    const FString Endpoint = FString::Format(*ServerEndpoint.endpoint, { PlayerId, FileId });
 
-    FString requestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(Endpoint.requestMethod));
-    HttpClient->SendApi(newEndpoint, requestMethod, ContentString, sessionResponse, true);
+    const FString RequestMethod = ULootLockerServerConfig::GetEnum(TEXT("ELootLockerServerHTTPMethod"), static_cast<int32>(ServerEndpoint.requestMethod));
+    const FString ContentString;
+    HttpClient->SendApi(Endpoint, RequestMethod, ContentString, SessionResponse, true);
 }
