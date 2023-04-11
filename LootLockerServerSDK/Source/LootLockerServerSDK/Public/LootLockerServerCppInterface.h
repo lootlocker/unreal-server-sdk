3e0298548a0ff75abe75b0e0e9bd705cfaa1bb97
Erik Bylund <erik@lootlocker.io>
2023-04-11 14:48:51 +0200

    Rename managers to reflect their usage

--- LootLockerServerSDK/Source/LootLockerServerSDK/Public/LootLockerServerCppInterface.h
+++ LootLockerServerSDK/Source/LootLockerServerSDK/Public/LootLockerServerCppInterface.h
@@ -3,29 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ServerAPI/LootLockerServerAuthRequest.h"
-#include "ServerAPI/LootLockerServerLeaderboardRequest.h"
+#include "LootLockerServerSDK/Private/ServerAPI/LootLockerServerAuthRequest.h"
+#include "LootLockerServerSDK/Private/ServerAPI/LootLockerServerLeaderboardRequest.h"
 
-#include "LootLockerServerManager.generated.h"
+#include "LootLockerServerCppInterface.generated.h"
 
-UCLASS(Blueprintable)
-class LOOTLOCKERSERVERSDK_API ULootLockerServerManager : public UObject
+class LOOTLOCKERSERVERSDK_API ULootLockerServerCppInterface : public UObject
 {
-    GENERATED_BODY()
- 
 public:
+    
     //==================================================
     // Authentication https://ref.lootlocker.com/server-api/#authentication
     //==================================================
-
+    
     /**
      * Start a session connecting to the LootLocker services with the server API key
      * https://ref.lootlocker.com/server-api/#registering-a-server-session
      *
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Authentication")
-    static void StartSession(const FLootLockerServerAuthResponseBP& OnCompletedRequest);
+	static void StartSession(const FLootLockerServerAuthResponseDelegate& OnCompletedRequest);
 
     /**
      * Keep the session alive, you should call this endpoint at least once per hour, to extend your tokens lifetime.
@@ -33,8 +30,7 @@ public:
      *
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Authentication")
-    static void MaintainSession(const FLootLockerServerMaintainSessionResponseBP& OnCompletedRequest);
+    static void MaintainSession(const FLootLockerServerMaintainSessionResponseDelegate& OnCompletedRequest);
 
     //==================================================
     // Leaderboards https://ref.lootlocker.com/server-api/#leaderboards
@@ -53,8 +49,7 @@ public:
      * @param OverwriteScoreOnSubmit Submitting a new score for member will always overwrite their existing score on leaderboard
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Leaderboards")
-    static void CreateLeaderboard(FString LeaderboardKey, FString Name, ELootLockerServerLeaderboardType Type, bool HasMetadata, ELootLockerServerLeaderboardDirection DirectionMethod, bool EnableGameApiWrites, bool OverwriteScoreOnSubmit, const FLootLockerServerCreateLeaderboardResponseBP& OnCompletedRequest);
+    static void CreateLeaderboard(FString LeaderboardKey, FString Name, ELootLockerServerLeaderboardType Type, bool HasMetadata, ELootLockerServerLeaderboardDirection DirectionMethod, bool EnableGameApiWrites, bool OverwriteScoreOnSubmit, const FLootLockerServerCreateLeaderboardResponseDelegate& OnCompletedRequest);
 
     /**
      * Update an existing leaderboard with the provided details.
@@ -63,13 +58,12 @@ public:
      * @param LeaderboardKey The key of the leaderboard to update
      * @param NewLeaderboardKey The unique key to set for the leaderboard, if you do not want to change it then set it to the same as LeaderboardKey
      * @param Name Name of the leaderboard
-     * @param DirectionMethod sort order (Ascending or Descending), based on whether highest rank is lowest or highest number
+     * @param DirectionMethod Sort order (Ascending or Descending), based on whether highest rank is lowest or highest number
      * @param EnableGameApiWrites Whether the Game API is permitted to write to this leaderboard
      * @param OverwriteScoreOnSubmit Submitting a new score for member will always overwrite their existing score on leaderboard
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Leaderboards")
-    static void UpdateLeaderboard(FString LeaderboardKey, FString NewLeaderboardKey, FString Name, ELootLockerServerLeaderboardDirection DirectionMethod, bool EnableGameApiWrites, bool OverwriteScoreOnSubmit, const FLootLockerServerUpdateLeaderboardResponseBP& OnCompletedRequest);
+    static void UpdateLeaderboard(FString LeaderboardKey, FString NewLeaderboardKey, FString Name, ELootLockerServerLeaderboardDirection DirectionMethod, bool EnableGameApiWrites, bool OverwriteScoreOnSubmit, const FLootLockerServerUpdateLeaderboardResponseDelegate& OnCompletedRequest);
 
     /**
      * Delete an existing leaderboard
@@ -78,8 +72,7 @@ public:
      * @param LeaderboardKey The key of the leaderboard to delete
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Leaderboards")
-    static void DeleteLeaderboard(FString LeaderboardKey, const FLootLockerServerDeleteLeaderboardResponseBP& OnCompletedRequest);
+    static void DeleteLeaderboard(FString LeaderboardKey, const FLootLockerServerDeleteLeaderboardResponseDelegate& OnCompletedRequest);
 
     /**
      * Submit a score to the given leaderboard
@@ -91,8 +84,7 @@ public:
      * @param Metadata Metadata to add to the score (will only be used if the leaderboard has metadata enabled)
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Leaderboards")
-    static void SubmitScore(FString LeaderboardKey, FString MemberID, int Score, FString Metadata, const FLootLockerServerLeaderboardSubmitScoreResponseBP& OnCompletedRequest);
+    static void SubmitScore(FString LeaderboardKey, FString MemberID, int Score, FString Metadata, const FLootLockerServerLeaderboardSubmitScoreResponseDelegate& OnCompletedRequest);
 
     /**
      * For all leaderboards that this member has scores on, get the score, member information, rank, score, and metadata (if metadata is enabled on that leaderboard), as well as player information if the leaderboard is of type player.
@@ -101,16 +93,15 @@ public:
      * @param MemberID The ID of the member to submit the score for
      * @param Count Number of members returned per page
      * @param After Cursor for pagination, a cursor will be returned in the response
-
+     
      * @param OnCompletedRequest Delegate for handling the response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Leaderboards")
-    static void GetAllMemberRanks(FString MemberID, const int Count, const int After, const FLootLockerServerGetAllMemberRanksResponseBP& OnCompletedRequest);
+    static void GetAllMemberRanks(FString MemberID, const int Count, const int After, const FLootLockerServerGetAllMemberRanksResponseDelegate& OnCompletedRequest);
 
     /**
      * From the requested leaderboard get <count> number of scores. The list of scores has member information, rank, score, and metadata (if metadata is enabled on that leaderboard) for the given leaderboard. If leaderboard is of type player a player will also be in the response.
      * Results are sorted in ascending order.
-     * Maximum allowed members to query for at a time is currently 2000.
+     * Maximum allowed members to query for at a time is currently 2000. 
      * https://ref.lootlocker.com/server-api/#get-score-list
      *
      * @param LeaderboardKey the key of the leaderboard you want to connect to.
@@ -118,6 +109,5 @@ public:
      * @param After Cursor for pagination, a cursor will be returned in the response
      * @param OnCompletedRequest Delegate for handling the server response
      */
-    UFUNCTION(BlueprintCallable, Category = "LootLockerServer Methods | Leaderboards")
-    static void GetScoresFromLeaderboard(FString LeaderboardKey, int Count, int After, const FLootLockerServerGetScoresFromLeaderboardResponseBP& OnCompletedRequest);
+    static void GetScoresFromLeaderboard(FString LeaderboardKey, int Count, int After, const FLootLockerServerGetScoresFromLeaderboardResponseDelegate& OnCompletedRequest);
 };
