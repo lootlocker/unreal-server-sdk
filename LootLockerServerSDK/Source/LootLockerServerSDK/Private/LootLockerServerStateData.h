b8e63a0e3e184d635cbaa99a5f266b2e4e13ecf8
Erik Bylund <erik@lootlocker.io>
2023-04-18 17:11:46 +0200

    Don't persist data to disk on server

--- LootLockerServerSDK/Source/LootLockerServerSDK/Private/LootLockerServerStateData.h
+++ LootLockerServerSDK/Source/LootLockerServerSDK/Private/LootLockerServerStateData.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
-#include "Launch/Resources/Version.h"
 #include "LootLockerServerStateData.generated.h"
 
 
@@ -14,17 +13,6 @@ class LOOTLOCKERSERVERSDK_API ULootLockerServerStateData : public UObject
 	GENERATED_BODY()
     static FString ServerToken;
 
-#if ENGINE_MAJOR_VERSION < 5
-	static const FString SaveSlot;
-    static constexpr int SaveIndex = 0;
-#else
-    inline static const FString SaveSlot = "LootLockerServer";
-    inline static constexpr int SaveIndex = 0;
-#endif
-	static bool StateLoaded;
-
-	static void LoadStateFromDiskIfNeeded();
-	static void SaveStateToDisk();
 public:
 	static FString GetServerToken();
 	static void SetServerToken(FString InServerToken);
