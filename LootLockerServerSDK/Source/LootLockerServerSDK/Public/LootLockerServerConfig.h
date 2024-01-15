8e738b5b6e56b4ed5ce48cc553bb103ff1ccca2a
Erik Bylund <erik@lootlocker.com>
2024-01-15 10:22:10 +0100

    Help users correctly use the game version setting

--- LootLockerServerSDK/Source/LootLockerServerSDK/Public/LootLockerServerConfig.h
+++ LootLockerServerSDK/Source/LootLockerServerSDK/Public/LootLockerServerConfig.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
-#include "HttpModule.h"
+#if ENGINE_MAJOR_VERSION >= 5
+#include <regex>
+#endif
 #include "LootLockerServerLogger.h"
 #include "LootLockerServerConfig.generated.h"
 
@@ -22,6 +23,8 @@ public:
 	FString LootLockerDomainKey = "";
 	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category = "LootLockerServer")
 	FString GameVersion = "";
+	UPROPERTY(Config, VisibleAnywhere, BlueprintReadOnly, Category = "LootLockerServer", Meta = (EditCondition = "!IsValidGameVersion", EditConditionHides), Meta = (MultiLine = true), Meta = (DisplayName = "WARNING:"), Transient)
+	FString InvalidGameVersionWarning = "Game version needs to follow a numeric Semantic Versioning pattern: X.Y.Z.B with the sections denoting MAJOR.MINOR.PATCH.BUILD and the last two being optional. Read more at https://docs.lootlocker.com/the-basics/core-concepts/glossary#game-version";
 	UPROPERTY(Config, VisibleAnywhere, BlueprintReadOnly, Meta = (EditCondition = "false", EditConditionHides), Category = "LootLockerServer")
 	FString LootLockerVersion = "2021-06-01";
 	// Limit the log level for the LootLocker Server SDK to this log level, will neither affect nor override the log level for the rest of your game 
@@ -38,6 +41,15 @@ public:
 	{
 		return LootLockerServerKey.Find("dev_", ESearchCase::CaseSensitive) == -1 && LootLockerServerKey.Find("prod_", ESearchCase::CaseSensitive) == -1;
 	}
+	UFUNCTION()
+	static bool IsSemverString(const FString& str)
+	{
+#if ENGINE_MAJOR_VERSION >= 5
+		return std::regex_match(TCHAR_TO_UTF8(*str), SemverPattern);
+#else
+		return true;
+#endif
+	}
 #if WITH_EDITOR
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override
 	{
@@ -45,15 +57,25 @@ public:
 		{
 			IsLegacyKey = IsLegacyAPIKey();
 		}
+		if (PropertyChangedEvent.GetPropertyName() == "GameVersion")
+		{
+			IsValidGameVersion = IsSemverString(GameVersion);
+		}
 		UObject::PostEditChangeProperty(PropertyChangedEvent);
 	}
 #endif //WITH_EDITOR
 	virtual void PostInitProperties() override
 	{
-		UObject::PostInitProperties();
 		IsLegacyKey = IsLegacyAPIKey();
+		IsValidGameVersion = IsSemverString(GameVersion);
+		UObject::PostInitProperties();
 	}
 private:
 	UPROPERTY(Config, VisibleInstanceOnly, Meta = (EditCondition = "false", EditConditionHides), Transient, Category = "LootLockerServer")
 	bool IsLegacyKey = false;
+	UPROPERTY(Config, VisibleInstanceOnly, Meta = (EditCondition = "false", EditConditionHides), Transient, Category = "LootLocker")
+	bool IsValidGameVersion = true;
+#if ENGINE_MAJOR_VERSION >= 5
+	inline static const std::regex SemverPattern = std::regex("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*))?(?:\\.(0|[1-9]\\d*))?$");
+#endif
 };
