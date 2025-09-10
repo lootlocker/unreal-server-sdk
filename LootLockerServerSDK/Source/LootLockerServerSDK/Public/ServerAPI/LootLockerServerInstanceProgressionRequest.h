28635ec96fb6b6a129ae81a0c2ebd5cc1a68d785
Erik Bylund <kirre.bylund@gmail.com>
2025-09-10 17:32:09 +0200

    feat: Add progression ids to all progression responses

--- LootLockerServerSDK/Source/LootLockerServerSDK/Public/ServerAPI/LootLockerServerInstanceProgressionRequest.h
+++ LootLockerServerSDK/Source/LootLockerServerSDK/Public/ServerAPI/LootLockerServerInstanceProgressionRequest.h
@@ -18,46 +18,51 @@ USTRUCT(BlueprintType)
 struct FLootLockerServerInstanceProgression
 {
 	GENERATED_BODY()
-		/*
-		 The ID of the progression
-		*/
-		UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Id = "";
+	/*
+	 The ID of the progression
+	*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
+	FString Id = "";
 	/*
 	 The key of the progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Progression_key = "";
+	FString Progression_key = "";
 	/*
 	 The name of the progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Progression_name = "";
+	FString Progression_name = "";
+	/*
+	 The ULID of the progression
+	*/
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
+    FString Progression_id = "";
 	/*
 	 Which step (steps are configured in the console) of the progression the instance is on
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Step = 0;
+	int32 Step = 0;
 	/*
 	 How many points this instance has in this progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Points = 0;
+	int32 Points = 0;
 	/*
 	 The number of points needed to enter the current step of the progression (already cleared by the instance)
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Previous_threshold = 0;
+	int32 Previous_threshold = 0;
 	/*
 	 The number of points needed to enter the next step of the progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Next_threshold = 0;
+	int32 Next_threshold = 0;
 	/*
 	 The time of the last level up
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Last_level_up = "";
+	FString Last_level_up = "";
 };
 
 //==================================================
@@ -68,73 +73,78 @@ USTRUCT(BlueprintType)
 struct FLootLockerServerSingleInstanceProgressionResponse : public FLootLockerServerResponse
 {
 	GENERATED_BODY()
-		/*
-		 The ID of the progression
-		*/
-		UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Id = "";
+	/*
+		The ID of the progression
+	*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
+	FString Id = "";
 	/*
 	 The key of the progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Progression_key = "";
+	FString Progression_key = "";
 	/*
 	 The name of the progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Progression_name = "";
+	FString Progression_name = "";
+	/*
+	 The ULID of the progression
+	*/
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
+    FString Progression_id = "";
 	/*
 	 Which step (steps are configured in the console) of the progression the instance is on
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Step = 0;
+	int32 Step = 0;
 	/*
 	 How many points this instance has in this progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Points = 0;
+	int32 Points = 0;
 	/*
 	 The number of points needed to enter the current step of the progression (already cleared by the instance)
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Previous_threshold = 0;
+	int32 Previous_threshold = 0;
 	/*
 	 The number of points needed to enter the next step of the progression
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		int32 Next_threshold = 0;
+	int32 Next_threshold = 0;
 	/*
 	 The time of the last level up
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FString Last_level_up = "";
+	FString Last_level_up = "";
 };
 
 USTRUCT(BlueprintType)
 struct FLootLockerServerSingleInstanceProgressionWithRewardsResponse : public FLootLockerServerSingleInstanceProgressionResponse
 {
 	GENERATED_BODY()
-		/*
-		 A list of the tiers that were awarded as an effect of the progression state change
-		*/
-		UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		TArray<FLootLockerServerProgressionTier> Awarded_tiers;
+	/*
+		A list of the tiers that were awarded as an effect of the progression state change
+	*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
+	TArray<FLootLockerServerProgressionTier> Awarded_tiers;
 };
 
 USTRUCT(BlueprintType)
 struct FLootLockerServerInstanceProgressionListResponse : public FLootLockerServerResponse
 {
 	GENERATED_BODY()
-		/*
-		 The current pagination data of the requested data
-		*/
-		UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		FLootLockerServerKeyBasedPagination Pagination;
+	/*
+		The current pagination data of the requested data
+	*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
+	FLootLockerServerKeyBasedPagination Pagination;
 	/*
 	 List of progression items according to the current pagination settings
 	*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLockerServer")
-		TArray<FLootLockerServerInstanceProgression> Items;
+	TArray<FLootLockerServerInstanceProgression> Items;
 };
 
 //==================================================
