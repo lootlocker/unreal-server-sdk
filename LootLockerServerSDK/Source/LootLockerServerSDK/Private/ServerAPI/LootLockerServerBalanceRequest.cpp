67142bbb7dd716e550660e35aea6d3d3f541f9ab
Erik Bylund <kirre.bylund@gmail.com>
2026-02-02 14:17:11 +0100

    refactor: Lift blueprint chain of Balances to BP interface

--- LootLockerServerSDK/Source/LootLockerServerSDK/Private/ServerAPI/LootLockerServerBalanceRequest.cpp
+++ LootLockerServerSDK/Source/LootLockerServerSDK/Private/ServerAPI/LootLockerServerBalanceRequest.cpp
@@ -10,57 +10,55 @@ ULootLockerServerBalanceRequest::ULootLockerServerBalanceRequest()
 {
 }
 
-void ULootLockerServerBalanceRequest::ListBalancesInWallet(const FString& WalletID, const FLootLockerServerListBalancesForWalletResponseBP& OnCompleteBP, const FLootLockerServerListBalancesForWalletResponseDelegate& OnComplete)
+void ULootLockerServerBalanceRequest::ListBalancesInWallet(const FString& WalletID, const FLootLockerServerListBalancesForWalletResponseDelegate& OnComplete)
 {
-	ULootLockerServerHttpClient::SendRequest<FLootLockerServerListBalancesForWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::ListBalancesInWallet, { WalletID }, {}, OnCompleteBP, OnComplete);
+	ULootLockerServerHttpClient::SendRequest<FLootLockerServerListBalancesForWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::ListBalancesInWallet, { WalletID }, {}, FLootLockerServerListBalancesForWalletResponseBP(), OnComplete);
 }
 
-void ULootLockerServerBalanceRequest::GetWalletByWalletID(const FString& WalletID, const FLootLockerServerGetWalletResponseBP& OnCompleteBP, const FLootLockerServerGetWalletResponseDelegate& OnComplete)
+void ULootLockerServerBalanceRequest::GetWalletByWalletID(const FString& WalletID, const FLootLockerServerGetWalletResponseDelegate& OnComplete)
 {
-	ULootLockerServerHttpClient::SendRequest<FLootLockerServerGetWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::GetWalletByWalletId, { WalletID }, {}, OnCompleteBP, OnComplete);
+	ULootLockerServerHttpClient::SendRequest<FLootLockerServerGetWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::GetWalletByWalletId, { WalletID }, {}, FLootLockerServerGetWalletResponseBP(), OnComplete);
 }
 
-void ULootLockerServerBalanceRequest::GetWalletByHolderID(const FString& HolderULID, const ELootLockerServerWalletHolderTypes& HolderType, const FLootLockerServerGetWalletResponseBP& OnCompleteBP, const FLootLockerServerGetWalletResponseDelegate& OnComplete)
+void ULootLockerServerBalanceRequest::GetWalletByHolderID(const FString& HolderULID, const ELootLockerServerWalletHolderTypes& HolderType, const FLootLockerServerGetWalletResponseDelegate& OnComplete)
 {
-	const auto Inspector = FLootLockerServerGetWalletResponseDelegate::CreateLambda([HolderULID, HolderType, OnCompleteBP, OnComplete](const FLootLockerServerGetWalletResponse& Response)
+	const auto Inspector = FLootLockerServerGetWalletResponseDelegate::CreateLambda([HolderULID, HolderType, OnComplete](const FLootLockerServerGetWalletResponse& Response)
 		{
 			if (Response.Success)
 			{
-				OnCompleteBP.ExecuteIfBound(Response);
 				OnComplete.ExecuteIfBound(Response);
 				return;
 			}
 
 			// Create Wallet and then get again
-			const auto CreateWalletResponseHandler = FLootLockerServerCreateWalletResponseDelegate::CreateLambda([HolderULID, OnCompleteBP, OnComplete, Response](const FLootLockerServerCreateWalletResponse& CreateWalletResponse)
+			const auto CreateWalletResponseHandler = FLootLockerServerCreateWalletResponseDelegate::CreateLambda([HolderULID, OnComplete, Response](const FLootLockerServerCreateWalletResponse& CreateWalletResponse)
 				{
 					if (!CreateWalletResponse.Success)
 					{
-						OnCompleteBP.ExecuteIfBound(Response);
 						OnComplete.ExecuteIfBound(Response);
 						return;
 					}
 
-					ULootLockerServerHttpClient::SendRequest<FLootLockerServerGetWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::GetWalletByHolderId, { HolderULID }, {}, OnCompleteBP, OnComplete);
+					ULootLockerServerHttpClient::SendRequest<FLootLockerServerGetWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::GetWalletByHolderId, { HolderULID }, {}, FLootLockerServerGetWalletResponseBP(), OnComplete);
 				});
-			CreateWallet(HolderULID, HolderType, FLootLockerServerCreateWalletResponseBP(), CreateWalletResponseHandler);
+			CreateWallet(HolderULID, HolderType, CreateWalletResponseHandler);
 		});
 	ULootLockerServerHttpClient::SendRequest<FLootLockerServerGetWalletResponse>(FLootLockerServerEmptyRequest{}, ULootLockerServerEndpoints::GetWalletByHolderId, { HolderULID }, {}, FLootLockerServerGetWalletResponseBP(), Inspector);
 }
 
-void ULootLockerServerBalanceRequest::CreditBalanceToWallet(const FString& WalletID, const FString& CurrencyID, const FString& Amount, const FLootLockerServerCreditWalletResponseBP& OnCompleteBP, const FLootLockerServerCreditWalletResponseDelegate& OnComplete)
+void ULootLockerServerBalanceRequest::CreditBalanceToWallet(const FString& WalletID, const FString& CurrencyID, const FString& Amount, const FLootLockerServerCreditWalletResponseDelegate& OnComplete)
 {
 
-	ULootLockerServerHttpClient::SendRequest<FLootLockerServerCreditWalletResponse>(FLootLockerServerCreditRequest{ Amount, CurrencyID, WalletID }, ULootLockerServerEndpoints::CreditBalanceToWallet, {}, {}, OnCompleteBP, OnComplete);
+	ULootLockerServerHttpClient::SendRequest<FLootLockerServerCreditWalletResponse>(FLootLockerServerCreditRequest{ Amount, CurrencyID, WalletID }, ULootLockerServerEndpoints::CreditBalanceToWallet, {}, {}, FLootLockerServerCreditWalletResponseBP(), OnComplete);
 }
 
-void ULootLockerServerBalanceRequest::DebitBalanceToWallet(const FString& WalletID, const FString& CurrencyID, const FString& Amount, const FLootLockerServerDebitWalletResponseBP& OnCompleteBP, const FLootLockerServerDebitWalletResponseDelegate& OnComplete)
+void ULootLockerServerBalanceRequest::DebitBalanceToWallet(const FString& WalletID, const FString& CurrencyID, const FString& Amount, const FLootLockerServerDebitWalletResponseDelegate& OnComplete)
 {
-	ULootLockerServerHttpClient::SendRequest<FLootLockerServerDebitWalletResponse>(FLootLockerServerDebitRequest{ Amount , CurrencyID, WalletID }, ULootLockerServerEndpoints::DebitBalanceToWallet, {}, {}, OnCompleteBP, OnComplete);
+	ULootLockerServerHttpClient::SendRequest<FLootLockerServerDebitWalletResponse>(FLootLockerServerDebitRequest{ Amount , CurrencyID, WalletID }, ULootLockerServerEndpoints::DebitBalanceToWallet, {}, {}, FLootLockerServerDebitWalletResponseBP(), OnComplete);
 }
 
-void ULootLockerServerBalanceRequest::CreateWallet(const FString& HolderULID, const ELootLockerServerWalletHolderTypes& HolderType, const FLootLockerServerCreateWalletResponseBP& OnCompleteBP, const FLootLockerServerCreateWalletResponseDelegate& OnComplete)
+void ULootLockerServerBalanceRequest::CreateWallet(const FString& HolderULID, const ELootLockerServerWalletHolderTypes& HolderType, const FLootLockerServerCreateWalletResponseDelegate& OnComplete)
 {
 	ULootLockerServerHttpClient::SendRequest<FLootLockerServerCreateWalletResponse>(FLootLockerServerCreateWalletRequest{ HolderULID, ULootLockerServerEnumUtils::GetEnum(TEXT("ELootLockerServerWalletHolderTypes"), static_cast<int32>(HolderType)).ToLower() },
-		ULootLockerServerEndpoints::CreateWallet, {}, {}, OnCompleteBP, OnComplete);
+		ULootLockerServerEndpoints::CreateWallet, {}, {}, FLootLockerServerCreateWalletResponseBP(), OnComplete);
 }
